4d9f732459d42c9726afb778f1a5a022d40656f4
21026584 <21026584@myrp.edu.sg>
2023-05-31 14:07:45 +0800

    -

--- login.php
+++ login.php
@@ -8,7 +8,6 @@
 </head>
 
 <body class="d-flex justify-content-center RootLoginBackground">
-<img src="Images/Classroom">
     <div class="d-flex justify-content-center RootLoginForm">
         <div class="Block">
         <h1 class="TitleText">Sign In</h1>
