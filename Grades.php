ca4c0aad103a11ac285bbabcaed47a7eeda95df0
21026584 <21026584@myrp.edu.sg>
2023-06-03 11:23:45 +0800

    Made changes to Grades page

--- Grades.php
+++ Grades.php
@@ -1,3 +1,12 @@
+<?php
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "c203_p09";
+$link = mysqli_connect($db_host,$db_username,$db_password,$db_name) or 
+        die(mysqli_connect_error());
+?>
+
 <!DOCTYPE html>
 <html>
 
