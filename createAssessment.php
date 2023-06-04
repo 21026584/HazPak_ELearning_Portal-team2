0ca66717045266dc8d667c3d36502c687648d44c
SimYuBo <21024095@myrp.edu.sg>
2023-06-04 16:04:38 +0800

    Assessment fix

--- createAssessment.php
+++ createAssessment.php
@@ -1,30 +1,27 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user_id'])) {
-    header("location: login.php");
-}
-?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="stylesheets/style.css" rel="stylesheet" type="text/css"/>
-        <title>Create Assessments</title>    
-    </head>
-    <body>
-        <?php include "navbar.php" ?>
-        <h3>Create Assessments</h3>
 
-        <form id="postForm" method="post" action="doCreateAssessment.php">
-            <label for="idName">Assessment Name:</label>
-            <input type="text" id="idName" name="name" required/>				
-            <label for="idInstuc">Instructions:</label>
-            <textarea id="idInstuc" name="instruction" rows="5" cols="30" required></textarea>	
-            <label for="idTime">Enter in release Time:</label>
-            <input type="datetime-local" id="idTime" name="time" required/>	
-            <p><label for="idQuestion">Questions:</label></p>
-            <textarea id="idQuestion" name="question" rows="4" cols="50" required></textarea>
-        </form>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Create Assessments</title>
+</head>
+
+<body>
+    <?php include "navbar.php" ?>
+    <h3>Create Assessments</h3>
+
+    <form id="postForm" method="post" action="doCreateAssessment.php">
+        <label for="idName">Assessment Name:</label>
+        <input type="text" id="idName" name="name" required />
+        <label for="idInstuc">Instructions:</label>
+        <textarea id="idInstuc" name="instruction" rows="5" cols="30" required></textarea>
+        <label for="idTime">Enter in release Time:</label>
+        <input type="datetime-local" id="idTime" name="time" required />
+        <p><label for="idQuestion">Questions:</label></p>
+        <textarea id="idQuestion" name="question" rows="4" cols="50" required></textarea>
+    </form>
+</body>
+
 </html>
