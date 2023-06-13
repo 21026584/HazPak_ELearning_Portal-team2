285d75ae95f122b58ed726f8f5733cc720b2853a
Evpyh <21021397@myrp.edu.sg>
2023-06-13 16:26:29 +0800

    updated takeAssessment.php , style & script

--- takeAssessment.php
+++ takeAssessment.php
@@ -6,51 +6,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
   <title>Take Assessment</title>
-  <style>
-    .collapsible {
-      background-color: #FADE1B;
-      color: black;
-      cursor: pointer;
-      padding: 18px;
-      width: 60vw;
-      border: none;
-      text-align: left;
-      outline: none;
-      font-size: 15px;
-      position: relative;
-      left: 5%
-    }
-
-    .active,
-    .collapsible:hover {
-      background-color: #F3DB2E;
-    }
-
-    .collapsible:after {
-      content: '\002B';
-      color: white;
-      font-weight: bold;
-      float: right;
-      margin-left: 5px;
-    }
-
-    .active:after {
-      content: "\2212";
-    }
-
-    .content {
-      padding-left: 1vw;
-      padding-right: 1vw;
-      max-height: 0;
-      overflow: hidden;
-      transition: max-height 0.2s ease-out;
-      background-color: #f1f1f1;
-      margin-bottom: 5vh;
-      width: 58vw;
-      position: relative;
-      left: 5%
-    }
-  </style>
 </head>
 
 <body>
@@ -73,24 +28,7 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
 
-  <script>
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + "px";
-        }
-      });
-    }
-  </script>
-
-
+  <script src="script.js"></script>
 </body>
 
 </html>
