f7cc26bc47706836f9c1f909be9973c37e21ee06
21026584 <21026584@myrp.edu.sg>
2023-06-13 16:11:18 +0800

    -

--- grades.php
+++ grades.php
@@ -28,8 +28,7 @@ mysqli_close($link);
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="dashboard-style.css">
-     <link rel="stylesheet" href="style.css">
+     <!-- <link rel="stylesheet" href="style.css"> -->
     <title> grades </title>
 </head>
 
