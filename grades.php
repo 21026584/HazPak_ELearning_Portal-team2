9924992a28728b5c708639387020241f99a89c0e
21026584 <104333830+21026584@users.noreply.github.com>
2023-06-19 15:43:23 +0800

    -

--- grades.php
+++ grades.php
@@ -28,7 +28,6 @@ mysqli_close($link);
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
- 
      <link rel="stylesheet" href="dashboard-style.css">
      <link rel="stylesheet" href="style.css">
     <title> grades </title>
@@ -100,34 +99,36 @@ for (i = 0; i < dropdown.length; i++) {
 }
 
 
-const data = {
-labels: $queryMembers,
-datasets: [{
-label: 'My First Dataset',
-backgroundColor: 'rgb(255, 99, 132)',
-borderColor: 'rgb(255, 99, 132)',
-data: [5, 10, 5, 2, 20, 30],
-}]
-};
-
-const config = {
-type: 'bar',
-data: data
-};
 
 const labels = [
-'January',
-'February',
-'March',
-'April',
-'May',
-'June'
-];
+     'Pass',
+     'Fail',
+    ];
 
-const myChart = new Chart(
-document.getElementById('myChart'),
-config
-);
+
+   const data = {
+     labels: labels,
+     datasets: [{
+       label: 'Dataset',
+       backgroundColor: [ 
+       'rgb(255, 99, 132)', 
+       'rgb(54, 162, 235)',
+
+     ],
+       data: [2,1],
+    }]
+   };
+
+    const config = {
+    type: 'bar',
+   data: data
+      };
+
+        const myChart = new Chart(
+      document.getElementById('myChart'),
+        config
+     );
+
 
 </script>
 </html>
