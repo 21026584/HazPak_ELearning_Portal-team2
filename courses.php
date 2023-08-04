9b0914f4514b3a5dd4ada47ed74678eca3255341
21026584 <21026584@myrp.edu.sg>
2023-08-04 10:59:42 +0800

    Made changes to courses page

--- courses.php
+++ courses.php
@@ -126,6 +126,34 @@ body{
   </div>
 
 </div>
+
+
+
+<button id="myBtn2">Add Course</button>
+
+<!-- The Modal -->
+<div id="myModal2" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+
+    <form id="AddCourseForm" method="post" action="doAddCourse.php">
+        <label for="courseId">Course Id:</label>
+        <input type="text" id="courseId" name="courseId" required />
+        <br>
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description" required />
+    <br>
+        <input type="submit" value="Add" />
+    </form>
+
+  </div>
+
+</div>
+
+
+
             <!-- Datatable -->
             <main class="tableMain">
                 <table id='exerciseTable' class="display table-striped data-table">
@@ -224,6 +252,33 @@ body{
 
 
 // Get the modal
+var modal = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -248,9 +303,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-
-
 
 
        
